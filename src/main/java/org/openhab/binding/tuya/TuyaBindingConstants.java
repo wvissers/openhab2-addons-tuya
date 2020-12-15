5e95eeddca0e2e2cca4558e76a11d13d8e880982
gvanhoecke <76070587+gvanhoecke@users.noreply.github.com>
2020-12-15 22:16:08 +0100

    Update TuyaBindingConstants.java

--- src/main/java/org/openhab/binding/tuya/TuyaBindingConstants.java
+++ src/main/java/org/openhab/binding/tuya/TuyaBindingConstants.java
@@ -26,6 +26,7 @@ public class TuyaBindingConstants {
     public final static ThingTypeUID THING_TYPE_COLOR_LED = new ThingTypeUID(BINDING_ID, "colorled");
     public final static ThingTypeUID THING_TYPE_FILAMENT_LED = new ThingTypeUID(BINDING_ID, "filamentled");
     public final static ThingTypeUID THING_TYPE_SIREN = new ThingTypeUID(BINDING_ID, "siren");
+    public final static ThingTypeUID THING_TYPE_CURTAIN_SWITCH = new ThingTypeUID(BINDING_ID, "curtainswitch");
 
     // List of all Channel ids.
     public final static String CHANNEL_POWER = "power";
